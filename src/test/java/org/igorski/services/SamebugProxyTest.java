de700a2d71fa83e9a1d1a072051d5643b8681c97
Igor Stojanovski <igorce@outlook.com>
2018-07-20 23:12:26 +0200

    Add SamebugProxy test

--- src/test/java/org/igorski/services/SamebugProxyTest.java
+++ src/test/java/org/igorski/services/SamebugProxyTest.java
@@ -2,6 +2,7 @@ package org.igorski.services;
 
 import org.igorski.clients.SamebugClient;
 import org.igorski.model.SamebugRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -9,8 +10,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
+import javax.ws.rs.ProcessingException;
 import java.util.Collections;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -26,10 +29,24 @@ class SamebugProxyTest {
     @Mock
     private SamebugClient samebugClientMock;
 
-    @Test
-    public void shouldUseRequestFactory() {
+    @BeforeEach
+    public void beforeEach() {
         when(requestFactoryMock.createInstance(throwableMock)).thenReturn(getRequestStub());
         when(samebugClientFactoryMock.getInstance()).thenReturn(samebugClientMock);
+    }
+
+    @Test
+    public void shouldUseRequestFactory() {
+        SamebugProxy proxy = new SamebugProxy(requestFactoryMock, samebugClientFactoryMock);
+
+        proxy.getSamebugRequest(throwableMock);
+        verify(requestFactoryMock).createInstance(throwableMock);
+    }
+
+    @Test
+    public void shouldHandlClientError() {
+        when(samebugClientMock.getCrashReport(any())).thenThrow(new ProcessingException("Procession exception"));
+
         SamebugProxy proxy = new SamebugProxy(requestFactoryMock, samebugClientFactoryMock);
 
         proxy.getSamebugRequest(throwableMock);
